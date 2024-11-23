e77f243b5a1063b6a1ba5eba46a758fe58f5d6f6
vipin172000 <vipinnkumar143@gmail.com>
2024-11-23 14:16:26 +0530

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,50 @@
+import java.nio.file.*;
+import java.util.*;
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        System.out.print("$ ");
-
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+  public static void main(String[] args) throws Exception {
+    Set<String> commands = Set.of("echo", "exit", "type");
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.print("$ ");
+      String input = scanner.nextLine();
+      if (input.equals("exit 0")) {
+        System.exit(0);
+      } else if (input.startsWith("echo ")) {
+        System.out.println(input.substring(5));
+      } else if (input.startsWith("type ")) {
+        String arg = input.substring(5);
+        if (commands.contains(arg)) {
+          System.out.printf("%s is a shell builtin%n", arg);
+        } else {
+          String path = getPath(arg);
+          if (path == null) {
+            System.out.printf("%s: not found%n", arg);
+          } else {
+            System.out.printf("%s is %s%n", arg, path);
+          }
+        }
+      } else {
+        System.out.printf("%s: command not found%n", input);
+        String command = input.split(" ")[0];
+        String path = getPath(command);
+        if (path == null) {
+          System.out.printf("%s: command not found%n", command);
+        } else {
+          String fullPath = path + input.substring(command.length());
+          Process p = Runtime.getRuntime().exec(fullPath.split(" "));
+          p.getInputStream().transferTo(System.out);
+        }
+      }
     }
+  }
+  private static String getPath(String command) {
+    for (String path : System.getenv("PATH").split(":")) {
+      Path fullPath = Path.of(path, command);
+      if (Files.isRegularFile(fullPath)) {
+        return fullPath.toString();
+      }
+    }
+    return null;
+  }
 }
